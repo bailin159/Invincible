11309dd1f1bf44e1adf0330846cda3c7ed7c18ae
dllo <929371786@qq.com>
2016-09-12 11:23:06 +0800

    genggai

--- app/src/main/java/com/example/bailin/invincible/Lsr.java
+++ app/src/main/java/com/example/bailin/invincible/Lsr.java
@@ -9,6 +9,8 @@ public class Lsr {
     // 来自 刘士壬 的第一次提交
     private int a;
     private int s;
+    private int e;
+    private String kaka;
 
     public int getA() {
         return a;
@@ -24,5 +26,31 @@ public class Lsr {
         this.s = s;
     }
 
+    public void setA(int a) {
+        this.a = a;
+    }
 
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public String getKaka() {
+        return kaka;
+    }
+
+    public void setKaka(String kaka) {
+        this.kaka = kaka;
+    }
+
+    public Lsr(String kaka) {
+        this.kaka = kaka;
+    }
 }
