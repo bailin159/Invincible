6989521c3fc7a95f52d1e79bb19cb8c1fd8068e6
白霖 <15915892@qq.com>
2016-09-12 11:10:23 +0800

    创建我的实体类

---
+++ app/src/main/java/com/example/bailin/invincible/BLTest.java
@@ -0,0 +1,25 @@
+package com.example.bailin.invincible;
+
+/**
+ * Created by bailin on 16/9/12.
+ */
+public class BLTest {
+    private String text;
+    private int index;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
